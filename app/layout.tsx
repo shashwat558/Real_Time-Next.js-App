702b954280a2f79c52681f6680e521591c61b9e7
shashwat558 <tumbaktuhere@gmail.com>
2024-08-20 20:48:50 +0530

    Fully set-up the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,29 @@ import "./globals.css";
 import { Inter as FontSans } from "next/font/google"
 
 import { cn } from "@/lib/utils"
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Your go to collaborrative text-editor"
+}
+
 export default function RootLayout({ children }:{children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   )
