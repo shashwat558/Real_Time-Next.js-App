75ea778d73218cd16b74e7c09e6c8326c6c32434
shashwat558 <tumbaktuhere@gmail.com>
2024-08-22 21:11:16 +0530

    Added server action to create a new room

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import {nanoid} from 'nanoid';
+import { liveblocks } from '../liveblocks';
+import { revalidatePath } from 'next/cache';
+import { parseStringify } from '../utils';
+
+export const createDocument = async ({userId, email}: CreateDocumentParams) => {
+    const roomId = nanoid()
+
+    try{
+        const metadata = {
+            creatorId: userId,
+            email, 
+            title: 'untitled'
+        }
+        const usersAccesses: RoomAccesses = {
+            [email]: ['room:write']
+        }
+
+
+
+        const room = await liveblocks.createRoom(roomId, {
+            metadata,
+            usersAccesses,
+            defaultAccesses:[]
+        } );
+
+        revalidatePath('/');
+
+        return parseStringify(room)
+
+    }catch(err){
+        console.log(`Error happened while creating a room: ${err}`)
+    }
+}
